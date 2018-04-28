aa0c8811f09bfbaae9e5b719afe5d86e04328dc2
xiuyuan chen <wr389110442@outlook.com>
2018-04-28 13:37:14 +0800

    添加内存和CPU总体利用率的获取,CPU存在问题,

--- Systream/Systream/SystreamInfo.cpp
+++ Systream/Systream/SystreamInfo.cpp
@@ -5,6 +5,7 @@
 SystreamInfo::SystreamInfo()
 {
 	memStatusex.dwLength = sizeof(MEMORYSTATUSEX);
+	Initialize();
 }
 
 
@@ -14,5 +15,54 @@ SystreamInfo::~SystreamInfo()
 
 int SystreamInfo::GetSytMemory()
 {
-	return memStatusex.dwMemoryLoad;
+	if (GlobalMemoryStatusEx(&memStatusex))
+	{
+		return memStatusex.dwMemoryLoad;
+	}
+	return 0;
+}
+
+int SystreamInfo::GetAllCpu()
+{
+	return 0;
+}
+
+bool SystreamInfo::Initialize()
+{
+	FILETIME ftIdle, ftKernel, ftUser;
+	double m_fOldCPUIdleTime;
+	double m_fOldCPUKernelTime;
+	double m_fOldCPUUserTime;
+	bool flag = false;
+	if (flag = GetSystemTimes(&ftIdle, &ftKernel, &ftUser))
+	{
+		m_fOldCPUIdleTime = FileTimeToDouble(ftIdle);
+		m_fOldCPUKernelTime = FileTimeToDouble(ftKernel);
+		m_fOldCPUUserTime = FileTimeToDouble(ftUser);
+	}
+	return flag;
+}
+
+int SystreamInfo::GetCpuUserRate()
+{
+	int nCPUUseRate = -1;
+	FILETIME ftIdle, ftKernel, ftUser;
+	if (GetSystemTimes(&ftIdle, &ftKernel, &ftUser))
+	{
+		double fCPUIdleTime = FileTimeToDouble(ftIdle);
+		double fCPUKernelTime = FileTimeToDouble(ftKernel);
+		double fCPUUserTime = FileTimeToDouble(ftUser);
+		nCPUUseRate = (int)(100.0 - (fCPUIdleTime - m_fOldCPUIdleTime)
+			/ (fCPUKernelTime - m_fOldCPUKernelTime + fCPUUserTime - m_fOldCPUUserTime)
+			*100.0);
+		m_fOldCPUIdleTime = fCPUIdleTime;
+		m_fOldCPUKernelTime = fCPUKernelTime;
+		m_fOldCPUUserTime = fCPUUserTime;
+	}
+	return nCPUUseRate;
+}
+
+double SystreamInfo::FileTimeToDouble(FILETIME & filetime)
+{
+	return (double)(filetime.dwHighDateTime * 4.294967296E9) + (double)filetime.dwLowDateTime;
 }
--- Systream/Systream/SystreamInfo.h
+++ Systream/Systream/SystreamInfo.h
@@ -9,11 +9,20 @@ public:
 	//获取内存使用率
 	int GetSytMemory();
 	//获取总CPU使用率
+	int GetAllCpu();
 	//获取单核CPU使用率
 	//获取总网络速率
 	//获取单个网卡网络速率
 
+	//CPU功能函数
+	bool Initialize();
+	int GetCpuUserRate();
+	double FileTimeToDouble(FILETIME &filetime);
+
 private:
 	MEMORYSTATUSEX memStatusex;
+	double m_fOldCPUIdleTime;
+	double m_fOldCPUKernelTime;
+	double m_fOldCPUUserTime;
 };
 
